6adca10a6d8c7b88e6612c81008c2ccabec8eb0e
Taco <verobere28@gmail.com>
2021-07-01 02:08:47 -0500

    added event.listeners to log in

--- pages/login.js
+++ pages/login.js
@@ -1,28 +1,58 @@
+import React, { useState } from "react";
+
 import s from "../styles/Login.module.scss";
+import Input from "../src/components/inputs";
 import Nav from "../src/components/navbarUser/index";
 import Footer from "../src/components/footerUser";
+//this allows the navbar to be sticked at top
+import Sticky from "react-sticky-el";
 
 const logIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const makeSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const logIn = {
+        email,
+        password,
+      };
+      await console.log(logIn);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className={s.background}>
-      <Nav />
-      <form className={s.form}>
+      <Sticky>
+        <Nav />
+      </Sticky>
+
+      <form onSubmit={makeSubmit} className={s.form}>
         <label for="email">Correo electrónico</label>
-        <input
+        <Input
           className={s.email}
-          id="email"
           type="email"
+          value={email}
+          callback={setEmail}
           placeholder="micorreo@correo.com"
         />
         <label for="password">Contraseña</label>
-        <input id="password" type="password" placeholder="**********" />
-        <input className={s.entra} id="logIn" type="submit" value="ENTRA" />
-        <input
-          className={s.registrar}
-          id="signIn"
-          type="submit"
-          value="REGÍSTRATE"
+        <Input
+          type="password"
+          value={password}
+          callback={setPassword}
+          placeholder="**********"
         />
+        <button className={s.entra} id="logIn" type="submit">
+          ENTRA
+        </button>
+        <button className={s.registrar} id="signIn" type="submit">
+          REGÍSTRATE
+        </button>
       </form>
       <Footer />
     </div>
--- src/components/navbarUser/index.jsx
+++ src/components/navbarUser/index.jsx
@@ -1,5 +1,48 @@
+import Image from "next/image";
+import Usuario from "./img/USUARIO.png";
+import s from "./Navbar.module.scss";
+import Drop from "./dropdownMenuNav/drop";
+
 const Nav = () => {
-  return <h3>navbar</h3>;
+  return (
+    <div>
+      <div className={s.navTop}>
+        <button className={s.btnLogo}></button>
+        <h3 className={s.location}>Tu ubicación: ---</h3>
+        <Drop className={s.drop} />
+        <input id="search-input" type="search" />
+        <button id="search" type="submit" />
+        <div className={s.user}>
+          <Image alt="" width={50} src={Usuario} />
+          <h3>Hola, aaaaaa</h3>
+        </div>
+        <div className={s.exit}>
+          <Image alt="" width={50} src={Usuario} />
+          <h3>Cerrar sesión</h3>
+        </div>
+      </div>
+      <div className={s.navBot}>
+        <ul>
+          <li>
+            <a>DIRECTORIO</a>
+          </li>
+          <li>
+            <a>CURSOS Y TALLERES</a>
+          </li>
+          <li>
+            <a>MARKETPLACE</a>
+          </li>
+          <li>
+            <a>COTIZA TU EVENTO</a>
+          </li>
+          <li>
+            <a>DONA ROPA Y ARTÍCULOS</a>
+          </li>
+        </ul>
+        <a>ACCESO A PROVEEDORES</a>
+      </div>
+    </div>
+  );
 };
 
 export default Nav;
