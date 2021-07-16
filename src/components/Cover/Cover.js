6979129708eaff8abd4c8c92a56e3488072cfd6c
eric-martinez <ericdemian.martinez@gmail.com>
2021-07-16 02:54:09 -0500

    ADDED Final design curso-sugerido

--- src/components/Cover/Cover.js
+++ src/components/Cover/Cover.js
@@ -1,38 +1,17 @@
-import CoverSuggestedCourses from '../CoverSuggestedCourses/CoverSuggestedCourses'
-import RoundButton from '../RoundButton/RoundButton'
-import PillButtonTrans from '../PillButtonTrans/PillButtonTrans'
-import CourseBar from '../CourseBar/CourseBar'
 import Styles from '../../../styles/Suggested.module.scss'
 import React from 'react'
+import { useRouter } from 'next/router'
 
-export default function Cover() {
+export default function Cover({ cursos }) {
+    const router = useRouter();
     return (
         <React.Fragment>
-            {
-                /*
-                    <div className={`${Styles.container}`}>
-                <div className={`${Styles.under}`}>
-                    <CoverSuggestedCourses/>
-                        <div className={`${Styles.ontop}`}>
-                            <h1>CURSO SUGERIDO</h1> 
-                            <p>Descripción del curso</p>
-                        </div>
-                        <div className={`${Styles.placing}`}>
-                            <RoundButton />
-                            <PillButtonTrans/>
-                        </div>
-                </div>
-                    <div></div>
-                </div>
-                <div></div>
-                
-                */
-            }
             <div className={Styles.headerWrapper}>
                 <div>
-                    <h3>Tus cursos</h3>
-                    <p>Aqui podras encontrar todos los cursos que has seleccionado o comprado.</p>
-                    <button>Ver mas</button>
+                    <h3>Curso Recomendado</h3>
+                    <p>{cursos.length === 0 ? null : cursos[0].title}</p>
+                    <p>{cursos.length === 0 ? null : cursos[0].description}</p>
+                    <button type="button" onClick={() => cursos.length === 0 ? null : router.push('/curso/' + cursos[0]._id)}>Ver mas</button>
                 </div>
             </div>
 
